b695677b672c9bd4b51a708f0aefed7dae29b44d
Robin Melanson <remcogold@gmail.com>
2019-06-01 22:38:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-# Setup the required environment
+#! /bin/bash
+# SETUP THE ENVIRONMENT
+setupName=setup.sh
+echo "$setupName: EXECUTING: . ./env/setEnv.sh $*"
 . ./env/setEnv.sh $*
-
 
 # COPY RESTORE 
 cp $pkg_RESTORE.sh ..
