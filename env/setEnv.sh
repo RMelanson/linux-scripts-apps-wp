fa8979ae535406b59cff7b4da01c1550ae704adb
Robin Melanson <robin.e.melanson@gmail.com>
2019-07-31 00:30:33 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -10,3 +10,12 @@ pkg=WORD_PRESS
 scriptType="apps"
 parentDir="/tmp/scripts/$scriptType/"
 installDir="$parentDir/$pkg"
+
+pkgOwner=ec2-user
+
+echo Setting External Args
+echo These Arguments Overwrite Default Argument Settings
+for arg in "$@"; do
+  echo setArgs EXECUTING: export $arg
+  export $arg
+done
